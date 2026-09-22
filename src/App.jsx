53fb7a555f279c5b7e5e81815075b39750d92b34
Lalit Bisht <lalitbisht0105@gmail.com>
2026-09-22 19:06:31 +0530

    Learning react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import UseStatePractice from './hooksPractice/useStatePractice'
+// import UseStateArr from './hooksPractice/useStateArr'
+// import Practice2 from './hooksPractice/Practice2'
+// import Practice2CF from './ControlledForms/Practice2CF'
+// import Counter from './hooksPractice/Counter_Hooks/Counter'
+// import Theme from './hooksPractice/Counter_Hooks/Theme'
+// import Timer from './hooksPractice/Use_Effect/Timer'
+// import Online from './hooksPractice/Counter_Hooks/Online'
+
+import Themes from "./hooksPractice/Theme/Theme2"
+
+import './App.css'
+
+function App() {
+  return (
+    <>  
+    <Themes/>
+    {/* <UseStateArr/> */}
+ </>
+  )
+}
+
+export default App
---
+++ src/hooksPractice/Theme/Theme2.jsx
@@ -0,0 +1,22 @@
+import UseTheme from "./useTheme"
+import "./Theme.css"
+
+const Theme2 = () => {
+  const theme = UseTheme()
+
+  return (
+    <div className={theme.theme}>
+      <div className="theme-card">
+        <h1>Theme</h1>
+
+        <button onClick={() => theme.toggle()}>
+          {theme.theme === "light" ? "dark" : "light"}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Theme2
+
+
